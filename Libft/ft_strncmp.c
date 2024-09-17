a4455f372a34e7abe0a90be82223da644347ca58
caarlitoos30 <167211404+caarlitoos30@users.noreply.github.com>
2024-09-17 23:10:35 +0200

    Add files via upload

---
+++ Libft/ft_strncmp.c
@@ -0,0 +1,39 @@
+
+
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while ((s1[i] != '\0' || s2[i] != '\0') && i < n)
+	{
+		if (s1[i] < s2[i])
+			return (-1);
+		else if (s1[i] > s2[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+/* #include <stdio.h>
+
+int main()
+{
+    char str1[] = "Hello";
+    char str2[] = "Hellowda";
+    unsigned int n = 8;
+    int result;
+
+    result = ft_strncmp(str1, str2, n);
+
+    if (result == 0)
+        printf("The strings are equal up to the first %u characters.\n", n);
+    else if (result < 0)
+        printf("First string is shorter to the first %u characters.\n", n);
+    else
+        printf("The first string is biggerto the first %u characters.\n", n);
+
+    return 0;
+}
+*/

7d4bca536a21ecf336098002bdf0a562db3157bf
Carlos Alguacil Garrido <calguaci@c3r10s5.42madrid.com>
2024-09-23 23:31:02 +0200

    23

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: calguaci <calguaci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 19:42:30 by calguaci          #+#    #+#             */
-/*   Updated: 2024/09/23 23:19:07 by calguaci         ###   ########.fr       */
+/*   Updated: 2024/09/23 23:26:16 by calguaci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,10 +34,11 @@ char			*ft_strchr(const char *str, size_t i);
 void			*ft_memchr(const void *s, int c, size_t n);
 void			*ft_memmove(void *dst, const void *src, size_t len);
 char			*ft_strrchr(const char *s, int c);
-int		        ft_memcmp(const void *s1, const void *s2, size_t n);
+int				ft_memcmp(const void *s1, const void *s2, size_t n);
 int				ft_atoi(const char *str);
 char			*ft_strnstr(const char *s1, const char *s2, size_t n);
 void			ft_putchar_fd(char c, int fd);
 void			ft_putendl_fd(char *s, int fd);
+void			ft_striteri(char *s, void (*f)(unsigned int, char*));
 
 #endif
